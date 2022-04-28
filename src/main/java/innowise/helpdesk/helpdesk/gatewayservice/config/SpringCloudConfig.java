9b1131d2ce0285450669f6329f311374ff29c89c
SvetilnikSt <svetilnik.stezhkina@gmail.com>
2022-04-28 18:14:23 +0300

    Initial commit

---
+++ src/main/java/innowise/helpdesk/helpdesk/gatewayservice/config/SpringCloudConfig.java
@@ -0,0 +1,23 @@
+package innowise.helpdesk.helpdesk.gatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder
+                .routes()
+
+                .route("historys_route",
+                        route -> route.path("/api/historys/**")
+                                .uri("lb://history-service"))
+
+                .build();
+    }
+
+}
