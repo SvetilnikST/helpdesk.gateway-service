f3fd755367be7765198c7e0405c790460e71ca04
SvetilnikST <svetilnik.stezhkina@gmail.com>
2022-05-12 10:34:53 +0300

    Keycloak configuration

--- src/main/java/innowise/helpdesk/helpdesk/gatewayservice/Application.java
+++ src/main/java/innowise/helpdesk/helpdesk/gatewayservice/Application.java
@@ -2,10 +2,11 @@ package innowise.helpdesk.helpdesk.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
-@EnableEurekaClient
+@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/innowise/helpdesk/helpdesk/gatewayservice/config/SpringCloudConfig.java
+++ src/main/java/innowise/helpdesk/helpdesk/gatewayservice/config/SpringCloudConfig.java
@@ -1,26 +1,26 @@
 package innowise.helpdesk.helpdesk.gatewayservice.config;
 
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 public class SpringCloudConfig {
-
     @Bean
-    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
-        return builder
-                .routes()
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .pathMatchers("/actuator/**")
+                .permitAll()
+                .and()
+                .authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2Login(); // to redirect to oauth2 login page.
 
-                .route("historys_route",
-                        route -> route.path("/api/historys/**")
-                                .uri("lb://history-service"))
-                .route("keycloak_route",
-                        route->route.path("/api/keycloak/**")
-                                .uri("lb://helpdesk-security-keycloak"))
-
-                .build();
+        return http.build();
     }
 
 }
