145571672a8410a2db93f40f149312e98837c4fe
SvetilnikST <svetilnik.stezhkina@gmail.com>
2022-05-23 17:19:24 +0300

    Refactoring gatewayservice

--- src/main/java/innowise/helpdesk/helpdesk/gatewayservice/Application.java
+++ src/main/java/innowise/helpdesk/helpdesk/gatewayservice/Application.java
@@ -2,11 +2,10 @@ package innowise.helpdesk.helpdesk.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication(exclude = ReactiveUserDetailsServiceAutoConfiguration.class)
-@EnableDiscoveryClient
+@SpringBootApplication
+@EnableEurekaClient
 public class Application {
 
 	public static void main(String[] args) {
